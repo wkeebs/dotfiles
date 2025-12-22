4323c2a70ca614e2e75fe24bd074277aec3998de
Will Keeble <will.a.keeble@gmail.com>
2025-12-22 14:50:33 +1100

    customise tmux

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -12,6 +12,30 @@ return {
       require "configs.lspconfig"
     end,
   },
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
+  {
+    'numToStr/Comment.nvim',
+    opts = {
+        -- add any options here
+    }
+}
 
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
