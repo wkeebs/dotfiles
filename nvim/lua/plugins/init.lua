775156f2dd4f8595d087ff280f6e46b208f36a0d
Will Keeble <will.a.keeble@gmail.com>
2026-03-13 15:09:50 +1100

    feat: add Oil

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -35,6 +35,17 @@ return {
     opts = {
         -- add any options here
     }
+  },
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
   }
 
   -- test new blink
